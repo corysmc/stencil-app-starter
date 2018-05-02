a15e8e52bf264aea3dbf58c5a923714b023c76ae
Cory McArthur <cory.s.mcarthur@gmail.com>
2018-05-02 11:05:23 -0600

    Adding popover bug demo

--- src/components/app-profile/app-profile.tsx
+++ src/components/app-profile/app-profile.tsx
@@ -9,12 +9,30 @@ export class AppProfile {
 
   render() {
     return (
-      <div class="app-profile">
-        <p>
-          Hello! My name is {this.name}. My name was passed in through a route
-          param!
-        </p>
-      </div>
+      <ion-content>
+        <div class="app-profile">
+          <p>
+            Hello! My name is {this.name}. My name was passed in through a route
+            param!
+          </p>
+        </div>
+        <ion-list>
+          <ion-item>
+            <ion-label>Popover</ion-label>
+            <ion-select
+              id="customPopoverSelect"
+              interface="popover"
+              placeholder="Select One"
+            >
+              <ion-select-option value="brown">Brown</ion-select-option>
+              <ion-select-option value="blonde">Blonde</ion-select-option>
+              <ion-select-option value="black">Black</ion-select-option>
+              <ion-select-option value="red">Red</ion-select-option>
+            </ion-select>
+          </ion-item>
+        </ion-list>
+        
+      </ion-content>
     );
   }
 }
